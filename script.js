2a054583ab8133e7feb42ac9340d1dd570b27adf
Deepika-Amraj <139916984+Deepika-Amraj@users.noreply.github.com>
2025-02-17 21:27:41 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const contactBtn = document.getElementById("contactBtn");
+    
+    contactBtn.addEventListener("click", function () {
+        window.location.href = "mailto:Deepika.amraj145@gmail.com";
+    });
+});
